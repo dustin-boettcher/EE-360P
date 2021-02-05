2ebed263582ba9bbbe32c4a54e936459a7de0a58
dustin-boettcher <43124981+dustin-boettcher@users.noreply.github.com>
2021-02-04 19:21:01 -0600

    Implemented PMerge

--- hw1-template/PMerge.java
+++ hw1-template/PMerge.java
@@ -6,63 +6,93 @@ import java.util.*;
 import java.util.concurrent.*;
 
 
-public class PMerge{
-  public static void parallelMerge(int[] A, int[] B, int[]C, int numThreads){
-    // TODO: Implement your parallel merge function
-	int size;
-	if (A.length > B.length) size = A.length / numThreads;
-	else size = B.length / numThreads;
-	final ForkJoinPool forkJoinPool = 
-			  new ForkJoinPool(numThreads);
-	int maxA = A.length - 1; int maxB = B.length - 1; int maxC = C.length - 1;
-	for (int i = 0; i < numThreads; i++) {
-		int x; int y;
-		if (A.length > B.length) {
-			x = A.length - size;
-		    y = binarySearch(B, A[A.length - size]);
-		    merge(x, y, maxA, maxB, A, B, C, maxC);
-		    maxA = x - 1; maxB = y - 1;
-		    maxC -= (maxB- y) + (maxA - x);
-		}
-		
-		    
-		
-		
+public class PMerge extends RecursiveAction{
+	int[] A;
+	int[] B;
+	int[] C;
+	int numThreads;
+	int aBegin;
+	int aEnd;
+	int bBegin;
+	int bEnd;
+	
+	public PMerge(int[] A, int[] B, int[] C, int aBegin, int aEnd, int bBegin, int bEnd) {
+		this.A = A;
+		this.B = B;
+		this.C = C;
+		this.aBegin = aBegin;
+		this.aEnd = aEnd;
+		this.bBegin = bBegin;
+		this.bEnd = bEnd;
 	}
-  }
+	
+	public static void parallelMerge(int[] A, int[] B, int[]C, int numThreads){
+	  
+		ForkJoinPool pool = new ForkJoinPool(numThreads);
+		PMerge t1 = new PMerge(A, B, C, 0, A.length - 1, 0, B.length - 1);
+		pool.invoke(t1);
+		pool.shutdown();
+	}
   
-  static int binarySearch(int[] A, int x) {
-	  int min = 0; int max = A.length - 1;
-	  int middle = (min + max) / 2;
-	  while (min < max) {
-		  if (A[middle] < x) {
-			  min = middle + 1;
-			  middle = (min + max) / 2;
-		  }
-		  else {
-			  max = middle - 1;
-			  middle = (min + max) / 2;
-		  }
-	  }
-	  return middle;
-  }
-  
-  static void merge(int minX, int minY, int maxX, int maxY, int[] A, int[] B, int[] C, int c) {
-	  int x = maxX;
-	  int y = maxY;
-	  for (int i = 0; i < (maxY - minY) + (maxX - minX); i++) {
-		  if (A[x] > B[y]){
-			  C[c] = A[x];
-			  --x;
-			  --c;
-		  }
-		  else{
-			  C[c] = B[y];
-			  --y;
-			  --c;
-		  }
-	  }
-  }
-  
-}
+	@Override
+	protected void compute() {
+		int size = aEnd - aBegin + bEnd - bBegin + 2;
+		if (aEnd - aBegin <= 1) {
+			merge();
+			return;
+		}
+		int[] median = findMedian(0, size / 2);
+		PMerge t1 = new PMerge(A, B, C, aBegin, aBegin + median[0] - 1, bBegin, bBegin + median[1] - 1);
+		PMerge t2 = new PMerge(A, B, C, aBegin + median[0], aEnd, bBegin + median[1], bEnd);
+		invokeAll(t1, t2);
+	}	
 
+	void merge() {
+		//sequential merge for small array chunks
+		while(aBegin <= aEnd && bBegin <= bEnd) {
+			if (A[aBegin] < B[bBegin]) {
+				C[C.length - (aBegin + bBegin) - 1] = A[aBegin];
+				++aBegin;
+			}
+			else {
+				C[C.length - (aBegin + bBegin) - 1] = B[bBegin];
+				++bBegin;
+			}
+		}
+		for (int i = aBegin; i <= aEnd; i++)
+			C[C.length - (i + bBegin) - 1] = A[i];
+		for (int i = bBegin; i <= bEnd; i++)
+			C[C.length - (i + aBegin) - 1] = B[i];
+	}
+
+	int[] findMedian(int low, int high) {
+		int size = aEnd - aBegin + bEnd - bBegin + 2;
+		if (aEnd < aBegin) return new int[]{0, (size / 2)};
+		if (bEnd < bBegin) return new int[]{(size / 2), 0};
+	
+		int aElements = ((aEnd - aBegin + 1) / 2);
+		if (aElements < 1){ 
+			return new int[]{0, size / 2};
+		}
+		return new int[]{aElements, binarySearch(B, bBegin, bEnd, A[aBegin + aElements - 1]) - bBegin + 1};
+	}
+
+	int binarySearch(int[] arr, int first, int last, int value) {
+		//returns the last index in arr that is less than value
+		int i = (first + last) / 2;
+		if (arr[i] <= value) {
+			if (i >= last)
+				return i;
+			if (arr[i + 1] > value)
+				return i;
+			else
+				return binarySearch(arr, i + 1, last, value);
+		}
+		else {
+			if (i <= first)
+				return i - 1;
+			else 
+				return binarySearch(arr, first, i - 1, value);
+		}
+	}
+}
