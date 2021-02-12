b2ad6811a25dcb822705ba02b6fac4e4cf08dcef
dustin-boettcher <43124981+dustin-boettcher@users.noreply.github.com>
2021-02-12 15:06:11 -0600

    CyclicBarrier part a)

--- hw2-template/CyclicBarrier.java
+++ hw2-template/CyclicBarrier.java
@@ -1,18 +1,29 @@
 /*
- * EID's of group members
+ * dmb4377
  * 
  */
 import java.util.concurrent.Semaphore; // for implementation using Semaphores
 
 public class CyclicBarrier {
+	int parties;
+	Semaphore sem1;
+	Semaphore sem2;
 	
 	public CyclicBarrier(int parties) {
+		this.parties = parties;
+		this.sem1 = new Semaphore(parties);
+		this.sem2 = new Semaphore(parties);
 	}
 	
 	public int await() throws InterruptedException {
-           int index = 0;
 		
-          // you need to write this code
+           int index = 0;
+           sem1.acquire();
+		   while (sem1.availablePermits() != 0){}
+		   sem1.release();
+		   sem2.acquire();
+		   while (sem2.availablePermits() != 0) {}
+		   sem2.release();
 	    return index;
 	}
 }
