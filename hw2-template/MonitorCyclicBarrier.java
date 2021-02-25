fa3cf690ede47128dde6ac4fd2af632bdc48ecae
dustin-boettcher <43124981+dustin-boettcher@users.noreply.github.com>
2021-02-25 16:55:21 -0600

    Q1 part b

--- hw2-template/MonitorCyclicBarrier.java
+++ hw2-template/MonitorCyclicBarrier.java
@@ -5,13 +5,42 @@
 
 public class MonitorCyclicBarrier {
 	
+	int index = 0;
+	int parties;
+	int leaving;
+	
 	public MonitorCyclicBarrier(int parties) {
+		this.parties = parties;
+		this.leaving = 0;
 	}
 	
 	public int await() throws InterruptedException {
-           int index = 0;
-		
+		while (leaving > 0) {}
+        int retVal = updateIndex();
+        
+		while (index < parties - 1){}
+		if (retVal == parties - 1)
+			leaving = parties - 1;
+	
+		//if (retVal == parties - 1) 
+        	//index = 0;
           // you need to write this code
-	    return index;
+		decrementLeaving();
+		//if (leaving == 0)
+			//index = 0;
+		//while (leaving > 0) {}
+	    return retVal;
 	}
+	
+	public synchronized int updateIndex() {
+		index++;
+		return index - 1;
+	}
+	
+	public synchronized int decrementLeaving() {
+		leaving--;
+		if (leaving == 0) index = 0;
+		return leaving + 1;
+	}
+	
 }
