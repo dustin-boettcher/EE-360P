ac6fee00aa8b7e89366badcda8dbe30e75debcac
dustin-boettcher <43124981+dustin-boettcher@users.noreply.github.com>
2021-02-26 19:05:47 -0600

    1 a) final

--- hw2-template/CyclicBarrier.java
+++ hw2-template/CyclicBarrier.java
@@ -1,5 +1,6 @@
 /*
- * dmb4377
+ * UT-EID=dmb4377
+ * UT-EID=ji4399
  * 
  */
 import java.util.concurrent.Semaphore; // for implementation using Semaphores
@@ -8,22 +9,45 @@ public class CyclicBarrier {
 	int parties;
 	Semaphore sem1;
 	Semaphore sem2;
+	Semaphore sem3;
+	static int index = 0;
+	static boolean sem1Full = false;
+	static boolean sem2Full = false;
 	
 	public CyclicBarrier(int parties) {
 		this.parties = parties;
 		this.sem1 = new Semaphore(parties);
 		this.sem2 = new Semaphore(parties);
+		this.sem3 = new Semaphore(1);
 	}
 	
 	public int await() throws InterruptedException {
 		
-           int index = 0;
+           int retVal = index;
            sem1.acquire();
-		   while (sem1.availablePermits() != 0){}
+           if (sem1.availablePermits() == 0){
+        	   sem2Full = false;
+        	   sem1Full = true;
+           }
+           
+           sem3.acquire(); //synchronize incrementing index
+           index += 1;
+           sem3.release();
+		   while (! sem1Full){
+			   Thread.sleep(1);
+		   }
 		   sem1.release();
+		    
+		   //Stage 2
+		   sem2Full = false;
 		   sem2.acquire();
-		   while (sem2.availablePermits() != 0) {}
+		   if (sem2.availablePermits() == 0) {
+			   index = 0;
+			   sem1Full = false;
+			   sem2Full = true;
+		   }
+		   while (! sem2Full) {}
 		   sem2.release();
-	    return index;
+	    return retVal;
 	}
 }
