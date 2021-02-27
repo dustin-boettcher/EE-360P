b415b985d12161614a20970d1c167f549d02df50
dustin-boettcher <43124981+dustin-boettcher@users.noreply.github.com>
2021-02-26 21:18:40 -0600

    Q 1b

--- hw2-template/MonitorCyclicBarrier.java
+++ hw2-template/MonitorCyclicBarrier.java
@@ -1,46 +1,91 @@
 /*
- * EID's of group members
+ * UT-EID=dmb4377
+ * UT-EID=ji4399
  * 
  */
 
 public class MonitorCyclicBarrier {
 	
-	int index = 0;
-	int parties;
-	int leaving;
+	static int index = 0;
+	static int parties;
+	static int leaving;
+	static boolean endOfRound = false;
+	static Object o1;
+	static Object o2;
 	
 	public MonitorCyclicBarrier(int parties) {
 		this.parties = parties;
 		this.leaving = 0;
+		this.o1 = new Object();
+		this.o2 = new Object();
 	}
 	
-	public int await() throws InterruptedException {
-		while (leaving > 0) {}
+	/*public int await() throws InterruptedException {
+		while (leaving > 0) {
+			Thread.sleep(1);
+			
+		}
         int retVal = updateIndex();
-        
-		while (index < parties - 1){}
-		if (retVal == parties - 1)
-			leaving = parties - 1;
+        System.out.println(index);
+		waitForThreads();
+		System.out.println("Left Section " + retVal + " " + index);
+		//if (retVal == parties - 1)
+			//leaving = parties - 1;
 	
 		//if (retVal == parties - 1) 
         	//index = 0;
           // you need to write this code
-		decrementLeaving();
-		//if (leaving == 0)
-			//index = 0;
-		//while (leaving > 0) {}
+		decrementLeaving(retVal);
+		updateIndex();
 	    return retVal;
-	}
+	}*/
 	
 	public synchronized int updateIndex() {
-		index++;
+		if (! endOfRound) index++;
+		if (leaving == 0 && endOfRound) {
+			index = 0;
+			endOfRound = false;
+		}
 		return index - 1;
 	}
 	
-	public synchronized int decrementLeaving() {
+	public synchronized void decrementLeaving() {
 		leaving--;
-		if (leaving == 0) index = 0;
-		return leaving + 1;
+	}
+	
+	public static void waitForThreads() throws InterruptedException {
+		synchronized(o1) {
+			++index;
+			if (index == parties - 1) {
+				o1.notifyAll();
+				leaving = parties - 1;
+			}
+			System.out.println("index is " + index);
+			while (index < parties - 1){
+				o1.wait();
+			}
+		}
+		
+	}
+	
+	public static void BlockFromEntering() throws InterruptedException {
+		synchronized(o2) {
+			if (leaving == 0) {
+				o2.notifyAll();
+				index = 0;
+			}
+			while (leaving > 0) {
+				o2.wait();
+			}
+		}
+	}
+	
+	public int await() throws InterruptedException{
+		BlockFromEntering();
+		waitForThreads();
+		decrementLeaving();
+		
+		return 1;
 	}
 	
 }
